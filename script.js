4156565516eb8c8105bc363a1da681e8e32226d2
Gwilo <41574564+Gwiilo@users.noreply.github.com>
2022-10-05 17:25:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function lightMode() {
   var body = document.getElementsByTagName("body")[0];
   if (!body.classList.contains("changed")) {
-    // LIGHT  MODE
+    // LIGHT MODE
     body.style.setProperty("--color-background", "#E6E5E0");
     body.style.setProperty("--color-background-secondary", "#e4ded1");
     body.style.setProperty("--color-primary", "#888E96");
@@ -12,7 +12,7 @@ function lightMode() {
     body.style.setProperty("--color-frequent", "#a8dadc");
     body.classList.add("changed");
   } else {
-    // DARK  MODE
+    // DARK MODE
     body.style.setProperty("--color-background", "#0d1321");
     body.style.setProperty("--color-background-secondary", "#1B212E");
     body.style.setProperty("--color-primary", "#748CAB");
